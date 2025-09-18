4103e6115bd54ca1930d38511f3efd71d7f695f3
ita0ts <rafael.alfalci@gmail.com>
2025-09-18 20:31:02 -0300

    acabei

--- main.js
+++ main.js
@@ -8,17 +8,41 @@ document.addEventListener("change", (event) => {
     // maiusculo para minusculo
     
     for (i = 0; i <= arraycabulosa.length; i++) {
-
-        let inputDadosChar = inputdados.charCodeAt(i);
-
-        if (inputDadosChar > 122 && inputDadosChar < 97) 
+    const btnMaiusculo = document.getElementById("btnMaiusculo");
+    const btnMinusculo = document.getElementById("btnMinusculo");
+        var seiLa = inputdados.value;
+        let inputDadosChar = seiLa.charCodeAt(i);
+        btnMaiusculo.addEventListener('click', letrasEmMaiusculo =>{
+        if (inputDadosChar > 122 || inputDadosChar < 97) 
         {
-            document.getElementById("textoexibido").innerText = inputdados(i + 32);
-            console.log(toString(inputDadosChar + 32));
+
+            document.getElementById("textoexibido").innerText = String.fromCharCode(inputDadosChar + 32);
+            console.log(String.fromCharCode(inputDadosChar));
 
             
-        } 
-        
+        } else {
+            console.log("deu red");
+            console.log((inputDadosChar));
+            console.log(String.fromCharCode(inputDadosChar));
+            
+        }
+        });
+        btnMinusculo.addEventListener('click', letrasEmMinusculo =>{
+            if (inputDadosChar > 97 || inputDadosChar < 122) 
+            {
+    
+                document.getElementById("textoexibido").innerText = String.fromCharCode(inputDadosChar + 32);
+                console.log(String.fromCharCode(inputDadosChar));
+    
+                
+            } else {
+                console.log("deu red");
+                console.log((inputDadosChar));
+                console.log(String.fromCharCode(inputDadosChar));
+                
+            }
+            });
+
         // if (char(inputdados.charCodeAt(i)) < 90 && char(inputdados.charCodeAt(i)) < 65) 
         // {
 
